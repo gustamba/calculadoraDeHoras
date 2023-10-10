39a921f1e23c2850b326da86d2fc17adfc055aeb
Gustavo Miranda <96025432+gustamba@users.noreply.github.com>
2023-10-10 14:11:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function calcularHoras() {
     if(horaInicio.value == 0 || horaFinal.value == 0) {
         window.alert("Preencher os campos necessários")
     } else {
-        
-        resultado.innerHTML = "Ainda estamos trabalhando nisso!";
+        var array = horaInicio.value.split(":");
+        var seconds = (parseInt(array[0], 10) * 60 * 60) + (parseInt(array[1], 10) * 60) + parseInt(array[2], 10)
+        resultado.innerHTML = horaInicio.value;
     }
 }
